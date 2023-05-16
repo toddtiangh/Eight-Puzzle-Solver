ea1c9c3ef4e383018c8976321cef1499e47eb817
toddtiangh <114514937+toddtiangh@users.noreply.github.com>
2023-05-16 07:22:54 -0400

    Update eight_puzzle.py

--- eight_puzzle.py
+++ eight_puzzle.py
@@ -3,16 +3,6 @@ from searcher import *
 from timer import *
 
 def create_searcher(algorithm, param):
-    """ a function that creates and returns an appropriate
-        searcher object, based on the specified inputs. 
-        inputs:
-          * algorithm - a string specifying which algorithm the searcher
-              should implement
-          * param - a parameter that can be used to specify either
-            a depth limit or the name of a heuristic function
-        Note: If an unknown value is passed in for the algorithm parameter,
-        the function returns None.
-    """
     searcher = None
     
     if algorithm == 'random':
@@ -32,14 +22,6 @@ def create_searcher(algorithm, param):
     return searcher
 
 def eight_puzzle(init_boardstr, algorithm, param):
-    """ a driver function for solving Eight Puzzles using state-space search
-        inputs:
-          * init_boardstr - a string of digits specifying the configuration
-            of the board in the initial state
-          * algorithm - a string specifying which algorithm you want to use
-          * param - a parameter that is used to specify either a depth limit
-            or the name of a heuristic function
-    """
     init_board = Board(init_boardstr)
     init_state = State(init_board, None, 'init')
     searcher = create_searcher(algorithm, eight_puzzle('125637084','Greedy',-1))
