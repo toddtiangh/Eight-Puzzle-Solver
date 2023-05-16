fdd67455d521df01ecbdebfd1af1cde670c32747
toddtiangh <114514937+toddtiangh@users.noreply.github.com>
2023-05-16 07:23:50 -0400

    Update state.py

--- state.py
+++ state.py
@@ -1,14 +1,8 @@
 from board import *
 
-# the list of possible moves, each of which corresponds to
-# moving the blank cell in the specified direction
 MOVES = ['up', 'down', 'left', 'right']
 
 class State:
-    """ A class for objects that represent a state in the state-space 
-        search tree of an Eight Puzzle.
-    """
-
     def __init__(self, board, predecessor, move):
 
         self.board = board
@@ -52,9 +46,7 @@ class State:
             print(self.board)
 
     def __repr__(self):
-        """ returns a string representation of the State object
-            referred to by self.
-        """
+
         # You should *NOT* change this method.
         s = self.board.digit_string() + '-'
         s += self.move + '-'
@@ -62,11 +54,6 @@ class State:
         return s
     
     def creates_cycle(self):
-        """ returns True if this State object (the one referred to
-            by self) would create a cycle in the current sequence of moves,
-            and False otherwise.
-        """
-        # You should *NOT* change this method.
         state = self.predecessor
         while state != None:
             if state.board == self.board:
@@ -75,11 +62,4 @@ class State:
         return False
 
     def __gt__(self, other):
-        """ implements a > operator for State objects
-            that always returns True. This will be needed to break
-            ties when we use max() on a list of [priority, state] pairs.
-            If we don't have a > operator for State objects,
-            max() will fail with an error when it tries to compare
-            two [priority, state] pairs with the same priority.
-        """
         return True
